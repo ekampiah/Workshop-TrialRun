ed2a93864f33eab6bc9b0f50b64d2d8ccf66f528
ekampiah <mr.ampiah@outlook.com>
2024-11-24 08:27:01 +0000

    remove weatherforecast elements and add typescript (#1)

--- Todo/Controllers/TodosController.cs
+++ Todo/Controllers/TodosController.cs
@@ -5,8 +5,23 @@ namespace Todo.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class TodosController : ControllerBase{
+    /// <summary>
+    /// Test Get operation
+    /// </summary>
+    /// <returns>string response if succesful</returns>
     [HttpGet]
-    public string TestMethod(){
-        return "Testicles";
+    [Produces("text/plain")]
+    public ActionResult<string> TestMethod(){
+        return Ok("Testicles");
+    }
+
+    /// <summary>
+    /// Test POST operation
+    /// </summary>
+    /// <param name="text">The input text</param>
+    /// <returns>string containing input text</returns>
+    [HttpPost]
+    public ActionResult TestPost([FromBody] string text){
+        return Ok($"Received {text}");
     }
 }
